0f35bb343df7584bd5fe387fb896e3fd73f7c56e
Sana Deghat <Sana.Deghat@gmail.com>
2026-08-15 07:49:23 -0700

    basuc player animation

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var gravity: Vector2 = Vector2(0.0, 980.0)
+var gravity: Vector2 = Vector2(0.0, 1000.0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,12 +11,11 @@ func get_gravity() -> Vector2:
 
 func set_gravity (new_gravity: Vector2):
 	gravity=new_gravity
-func change_gravity(mode:int) -> void:
-	if (mode==0):
-		gravity.y*=-1
-	elif (mode==1):
-		gravity.x*=-1
-	gravity= Vector2(0.0, 980.0)
+func change_gravity() -> void:
+	print("change")
+	
+	gravity.y*=-1
+	gravity.x*=-1
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,24 +1,37 @@
 extends CharacterBody2D
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += global.get_gravity() * delta
+	# Gravity
+	velocity += global.get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	# Gravity change + spin
+	if Input.is_action_just_pressed("ui_accept"):
+		global.change_gravity()
+		animated_sprite_2d.play("air-spin")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Movement
 	var direction := Input.get_axis("ui_left", "ui_right")
+
 	if direction:
 		velocity.x = direction * SPEED
+
+		if direction < 0:
+			animated_sprite_2d.flip_h = true
+		else:
+			animated_sprite_2d.flip_h = false
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+	# Only change animation if we're NOT currently doing the spin
+	if animated_sprite_2d.animation != "air-spin" or not animated_sprite_2d.is_playing():
+		if direction:
+			animated_sprite_2d.play("walk")
+		else:
+			animated_sprite_2d.play("idle")
